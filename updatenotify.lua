738ffffefb65bc8005c70b12b96bffa081f5eb65
Brues <5278969+brues-code@users.noreply.github.com>
2026-07-25 19:16:02 -0500

    Refactor version checking and update addon prefix
    Update version to use build system template placeholder (@project-version@). Refactor updatenotify.lua to:
    
    - Skip initialization when using development version placeholder
    - Introduce ADDON_PREFIX constant for version check messages
    - Consolidate CHAT_MSG_ADDON event handling logic
    - Update update notification URL to GitHub releases page

--- updatenotify.lua
+++ updatenotify.lua
@@ -1,4 +1,7 @@
-local function strsplit(delimiter, subject)
+local version = GetAddOnMetadata("pfQuest", "Version")
+if version == "@project-version@" then return end
+
+local strsplit = strsplit or function(delimiter, subject)
   if not subject then
     return nil
   end
@@ -10,6 +13,9 @@ local function strsplit(delimiter, subject)
   return unpack(fields)
 end
 
+
+local ADDON_PREFIX = "pfQuest-CAPI"
+
 local channels = { "BATTLEGROUND", "RAID", "GUILD" }
 local version, remote, major, minor, fix, displayed, available
 local versioncheck = CreateFrame("Frame")
@@ -20,7 +26,7 @@ versioncheck:RegisterEvent("PLAYER_ENTERING_WORLD")
 versioncheck:SetScript("OnEvent", function()
   if event == "ADDON_LOADED" then
     if arg1 == "pfQuest" then
-      major, minor, fix = strsplit(".", tostring(GetAddOnMetadata(arg1, "Version")))
+      major, minor, fix = strsplit(".", tostring(version))
       major = tonumber(major) or 0
       minor = tonumber(minor) or 0
       fix = tonumber(fix) or 0
@@ -29,17 +35,17 @@ versioncheck:SetScript("OnEvent", function()
     end
 
     return
-  elseif event == "CHAT_MSG_ADDON" and arg1 == "pfQuest" then
-    local v, remoteversion = strsplit(":", arg2)
-    local remoteversion = tonumber(remoteversion)
-    if v == "VERSION" and remoteversion then
-      remote = remote and max(remote, remoteversion) or remoteversion
-      if remote > version then
-        pfQuest_config.latest = remote
+  elseif event == "CHAT_MSG_ADDON" then
+    if arg1 == ADDON_PREFIX then
+      local v, remoteversion = strsplit(":", arg2)
+      remoteversion = tonumber(remoteversion)
+      if v == "VERSION" and remoteversion then
+        remote = remote and max(remote, remoteversion) or remoteversion
+        if remote > version then
+          pfQuest_config.latest = remote
+        end
       end
     end
-    return
-  elseif event == "CHAT_MSG_ADDON" then
     return
   end
 
@@ -50,7 +56,7 @@ versioncheck:SetScript("OnEvent", function()
 
   -- send updates
   for _, chan in pairs(channels) do
-    SendAddonMessage("pfQuest", "VERSION:" .. version, chan)
+    SendAddonMessage(ADDON_PREFIX, "VERSION:" .. version, chan)
   end
 
   -- abort here on group member events
@@ -61,7 +67,7 @@ versioncheck:SetScript("OnEvent", function()
   -- display available update
   if version and version > 0 and pfQuest_config.latest and pfQuest_config.latest > version and not displayed then
     DEFAULT_CHAT_FRAME:AddMessage(
-      pfQuest_Loc["|cff33ffccpf|rQuest: New version available! Have a look at http://shagu.org !"]
+      pfQuest_Loc["|cff33ffccpf|rQuest: New version available! Have a look at https://github.com/brues-code/pfQuest/releases/latest !"]
     )
     displayed = true
   end
